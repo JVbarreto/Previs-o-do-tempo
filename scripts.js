378beefda41db91e6e0f2420f4b0cd7221dce31e
Vitor Barreto <vitorbarreto1432@gmail.com>
2024-03-12 23:39:19 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+
+const key = "a392b623dc5d4822118c5d69b56db46e"
+
+function colocarDadosNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "% Umido"
+    document.querySelector(".img-prevesao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+}
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
